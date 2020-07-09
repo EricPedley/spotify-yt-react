cc2dc883145591ca0b43f05face7411bb1fb1920
ericpedley@gmail.com <ericpedley@gmail.com>
2020-07-08 21:47:05 -0700

    implementing login button functionality

--- src/spotify/SpotifyHalf.js
+++ src/spotify/SpotifyHalf.js
@@ -1,21 +1,29 @@
-import React from "react"
-export default function SpotifyHalf() {
+import React, { useState } from "react"
 
+function LoginButtons(props) {
     function showImportScreen() {
-        console.log("show import screen");
+        props.setState("importField");
     }
     return (
+        <>
+        <a href="/spotify-login" id="spotify-login" className="big-link spotify-colors">
+            <h3>Log in with Spotify</h3>
+        </a>
+        <div id="spotify-loggedin"></div>
+        <br></br>
+        <br></br>
+        <button id="import-button" onClick={showImportScreen} className="pressable big-link">
+            <h3>Or Import Tracks From JSON</h3>
+        </button>
+        <div id="import-holder"></div>
+        </>
+    )
+}
+export default function SpotifyHalf() {
+    const [state, setState] = useState("noAuth");
+    return (
         <div className="col-md-6 half" id="spotify-half">
-            <a href="/spotify-login" id="spotify-login" className="big-link spotify-colors">
-                <h3>Log in with Spotify</h3>
-            </a>
-            <div id="spotify-loggedin"></div>
-            <br></br>
-            <br></br>
-            <button id="import-button" onClick={showImportScreen} className="pressable big-link">
-                <h3>Or Import Tracks From JSON</h3>
-            </button>
-            <div id="import-holder"></div>
+            {state === "noAuth" && LoginButtons(state)}
         </div>
     );
 }
