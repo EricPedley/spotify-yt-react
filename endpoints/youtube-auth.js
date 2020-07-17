8dca775571265e32db22fee697bdb1931b034b3f
ericpedley@gmail.com <ericpedley@gmail.com>
2020-07-17 00:11:54 -0700

    added cookie expiration on youtube token

--- endpoints/youtube-auth.js
+++ endpoints/youtube-auth.js
@@ -5,17 +5,15 @@ var youtube_redirect_uri = process.env.YOUTUBE_REDIRECT_URI;
 var youtube_client_secret = process.env.YOUTUBE_CLIENT_SECRET;
 var oauth2Client = new OAuth2(youtube_client_id,youtube_client_secret,youtube_redirect_uri);
 var spotify_params;
-module.exports = {
+module.exports = {//TODO this probably breaks if multiple people are using it at the same time since there is only one oauth2 client for the whole server
     oauth2Client: oauth2Client,
     login: function (req, res) {
         spotify_params=req.query.spotify_params;
         res.redirect(oauth2Client.generateAuthUrl({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" }));
     },
-    callback: function (req, res) {
-        console.log(req.query.code);
-        oauth2Client.getToken(req.query.code, function (err, token) {
-            oauth2Client.setCredentials(token);
-        });
-        res.cookie("youtube-auth",true).redirect("http://localhost:3000");
+    callback: async (req, res) => {
+        const token = await oauth2Client.getToken(req.query.code);
+        oauth2Client.setCredentials(token);
+        res.cookie("youtube-auth",true,{expires:new Date(token.expiry_date)}).redirect("http://localhost:3000");
     }
 }
