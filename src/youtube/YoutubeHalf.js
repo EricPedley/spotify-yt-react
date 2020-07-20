a2d7d4a6b479787484b27f32e8600753fe106d35
ericpedley@gmail.com <ericpedley@gmail.com>
2020-07-20 11:36:11 -0700

    added logout button

--- src/youtube/YoutubeHalf.js
+++ src/youtube/YoutubeHalf.js
@@ -2,13 +2,13 @@ import React, { useState, useContext } from "react"
 import PlaylistContext from "../PlaylistContext";
 export default function YoutubeHalf() {
     let youtube_access_token = document.cookie.split("; ").find((row) => row.startsWith("youtube_access_token"));
-    const [state,setState] = useState(youtube_access_token?"playlistList":"noAuth");
+    const [state,setState] = useState(youtube_access_token&&youtube_access_token.length>21 ? "playlistList" : "noAuth");
     // console.log(document.cookie);
 
     return (
         <div className="col-md-6 half right-half">
-            {state==="noAuth" && <LoginButton />}
-            {state==="playlistList" && <PlaylistSelect token={youtube_access_token} />}
+            {state === "noAuth" && <LoginButton />}
+            {state === "playlistList" && <PlaylistSelect token={youtube_access_token.substring(21)} setParentState={setState} />}
         </div>
     );
 }
@@ -23,29 +23,36 @@ function LoginButton() {
 
 function PlaylistSelect(props) {
     const [state, setState] = useState({});
-    const [context,setContext]  = useContext(PlaylistContext);
+    const [context, setContext] = useContext(PlaylistContext);
     const options = {
         headers: {
-            Authorization: `Bearer ${props.token.substring(21)}`
+            Authorization: `Bearer ${props.token}`
         }
     };
-    if(!state.playlists)
-        fetch("https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true",options).then((res)=>res.json()).then((json)=>{
-            setState({...state,playlists:json.items});
-        });
+    if (!state.playlists)
+        fetch("https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true", options).then((res) => res.json()).then((json) => {
+            setState({ ...state, playlists: json.items });
+        }).catch(console.error);
     function setSelected(id) {
-        setContext({ytID:id, ...context});
+        setContext({ ytID: id, ...context });
         console.log(context);
     }
+
+    function logOut() {
+        document.cookie = new URLSearchParams({youtube_access_token:""});
+        props.setParentState("noAuth");
+        setContext({...context,ytID:null});
+    }
     return (
-                <div className="left-align">
-                    {!state.playlists&&<h2>Loading...</h2>}
-                    {state.playlists&&<><h2>Logged in as {state.playlists[0].snippet.channelTitle}</h2>
-                    {state.playlists.map((playlist) => (
-                        <button onClick={()=>{setSelected(playlist.id)}} className={`pressable small-link playlist-button ${(context.ytID===playlist.id?"youtube-colors":"no-background")}`} key={playlist.id}>{playlist.snippet.title}</button>
-                    ))}</>}
-                    
-                </div>
-            
+        <div className="left-align">
+            {!state.playlists && <h2>Loading...</h2>}
+            {state.playlists && <><h2>Logged in as {state.playlists[0].snippet.channelTitle}</h2>
+                <button className="pressable small-link logout-button" onClick={logOut}>Log Out</button>
+                {state.playlists.map((playlist) => (
+                    <button onClick={() => { setSelected(playlist.id) }} className={`pressable small-link playlist-button ${(context.ytID === playlist.id ? "youtube-colors" : "no-background")}`} key={playlist.id}>{playlist.snippet.title}</button>
+                ))}</>}
+
+        </div>
+
     );
 }
