eb05a36fca49d052487bb05aeb44974c4cdd99fd
ericpedley@gmail.com <ericpedley@gmail.com>
2020-08-08 15:51:45 -0700

    added options for puppeteer to work on heroku

--- endpoints/youtube-search.js
+++ endpoints/youtube-search.js
@@ -2,29 +2,34 @@ const puppeteer = require("puppeteer");
 
 let requests = 0;
 module.exports = {
-    search: async (req,res)=>{
+    search: async (req, res) => {
         console.log(req.query.term);
         console.log(++requests)
         const id = await search(req.query.term);
-        console.log(req.query.term+" : "+id);
-        res.send({id:id});
+        console.log(req.query.term + " : " + id);
+        res.send({ id: id });
     }
 }
 
 
 const search = async (query) => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ],
+        });//TODO: find a way for browser to not launch for every request
         const page = await browser.newPage();
         await page.goto(`https://www.youtube.com/results?search_query=${new URLSearchParams(query).toString()}`);
         const bruh = await page.$eval("#video-title", (element) => element.href);
         await page.close();
         await browser.close();
-        return bruh.substring(bruh.indexOf("?v=")+3);
-    } catch(error) {
-        console.log("error for query "+query);
+        return bruh.substring(bruh.indexOf("?v=") + 3);
+    } catch (error) {
+        console.log("error for query " + query);
         console.log(error);
         return -1;
     }
-    
+
 }
