779d2e6fb7142268db5453e1dbbc3e331f7908c6
ericpedley@gmail.com <ericpedley@gmail.com>
2020-09-07 21:33:17 -0700

    made total display in header

--- endpoints/db-connector.js
+++ endpoints/db-connector.js
@@ -2,10 +2,11 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:6qTykVhCrN3c3yBz@spotify-yt-converter.g0nux.azure.mongodb.net"//process.env.MONGO_URL;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 module.exports = {
     get: (req, res) => {
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         client.connect(async err => {
             if (err) {
                 console.log(err);
@@ -18,15 +19,17 @@ module.exports = {
             if (count === undefined) {
                 await collection.insertOne({ day: day, count: 10000 });
                 await client.close();
-                res.send(10000);
+                res.send("10000");
             } else {
                 await client.close();
-                res.send(count);
+                res.send(`${count}`);
             }
         });
     },
     patch: (req, res) => {//this is called when a youtube api call is made, and one of the url params is the amount of units spent
         const cost = req.query.cost;
+        console.log(cost);
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         client.connect(async err => {
             if (err)
                 console.log(err);
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import path from "./serverURL"
 
 function Header() {
     const [instructions,] = useState("Log in to select playlists");
+    const [units,setUnits] = useState();
+    fetch(`${path}quota-count`).then(res=>res.json()).then(setUnits);
     return (
         <div className="header">
-        <div className="container">
-            <div className="row">
-                <div className="col-3"><img id="logo" alt = "logo" src="images/icon.png"></img></div>
-                <div className="col-9">
-                    <h1>Spotify To Youtube Playlist Converter</h1>
-                    <div id='instructions'>{instructions}</div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3"><img id="logo" alt="logo" src="images/icon.png"></img></div>
+                    <div className="col-9">
+                        <h1>Spotify To Youtube Playlist Converter</h1>
+                        <div id='instructions'>{instructions}</div>
+                        <div>Quota Total: {units}</div>
+                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
         </div>
     );
 }
-
 export default Header;
