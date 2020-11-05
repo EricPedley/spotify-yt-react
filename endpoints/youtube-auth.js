9cb345f7ca1697393262b47a7ef9bbb9b8e5dbf0
ericpedley@gmail.com <ericpedley@gmail.com>
2020-11-05 15:35:55 -0800

    added reminder to update scope

--- endpoints/youtube-auth.js
+++ endpoints/youtube-auth.js
@@ -8,10 +8,10 @@ module.exports = {
             client_id:youtube_client_id,
             redirect_uri:youtube_redirect_uri,
             response_type:"code",
-            scope:"https://www.googleapis.com/auth/youtube.force-ssl"
+            scope:"https://www.googleapis.com/auth/youtube.force-ssl"//TODO change scope to use incremental auth
+            //https://developers.google.com/youtube/v3/guides/auth/server-side-web-apps#incrementalAuth
         };
         res.redirect("https://accounts.google.com/o/oauth2/v2/auth?"+new URLSearchParams(params).toString());
-        //res.redirect(oauth2Client.generateAuthUrl({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" }));
     },
     callback: async (req, res) => {
         const options = {
