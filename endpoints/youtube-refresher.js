770f506ed0acdb4940b652959b80539b8801f827
Eric Pedley <ericpedley@gmail.com>
2020-12-02 19:00:22 -0800

    fixed stupid error and added more useful error handling

--- endpoints/youtube-refresher.js
+++ endpoints/youtube-refresher.js
@@ -3,10 +3,9 @@ const youtube_api_key = process.env.YOUTUBE_API_KEY;
 module.exports = {
     refresh: async (req,res )=> {
         console.log("refreshing youtube api")
-        const ytres = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=qo3ewdnBDnA&maxResults=1&key=${youtube_api_key}bruh`).then(res=>res.json());
+        const ytres = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=qo3ewdnBDnA&maxResults=1&key=${youtube_api_key}dumbass`).then(res=>res.json());
         if(ytres.error)
-            res.sendStatus(ytres.error.code)
-        else
-            res.send(ytres);
+            console.log("error:",ytres)
+        res.status(500).send(ytres);
     }
 }
