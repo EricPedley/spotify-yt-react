edf5b1ac000eb15e8c50a5d30fd500541fdfcbc3
Eric Pedley <ericpedley@gmail.com>
2021-10-11 16:20:56 -0700

    fixed serverless refresh endpoint

--- api/youtube/refresh.js
+++ api/youtube/refresh.js
@@ -2,8 +2,11 @@ const fetch = require('node-fetch');
 const youtube_api_key = process.env.YOUTUBE_API_KEY;
 export default async (req,res )=> {
     console.log("refreshing youtube api")
-    const ytres = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=qo3ewdnBDnA&maxResults=1&key=${youtube_api_key}dumbass`).then(res=>res.json());
-    if(ytres.error)
-        console.log("error:",ytres)
-    res.status(500).send(ytres);
+    const ytres = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=qo3ewdnBDnA&maxResults=1&key=${youtube_api_key}`).then(res=>res.json());
+    let statusCode = 200;
+    if(ytres.error) {
+        console.log("error:",ytres,youtube_api_key)
+        statusCode=500;
+    }
+    res.status(statusCode).send(ytres);
 }
